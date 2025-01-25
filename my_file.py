6de472de8df07f9c4e07a7384975263038865dd6
vonapetz <stepan0via@ya.ru>
2025-01-25 13:20:53 +0300

    Обновление

--- my_file.py
+++ my_file.py
@@ -1,5 +1,20 @@
-<<<<<<< HEAD
+import asyncio
+import time
+
 print('This file was cloned from my repository on GitHub!')
-=======
-print('This file was cloned from my repository on GitHub!')
->>>>>>> 0847322b30af557ec40addf74dc3533dd1e86094
+
+# это пример блокирующего кода
+async def send_mail(num):
+    print(f'Улетело сообщение {num}')
+    await asyncio.sleep(1)  # Имитация отправки сообщения по сети
+    print(f'Сообщение {num} доставлено')
+
+
+async def main():
+    for i in range(10):
+        await send_mail(i)
+
+
+start_time = time.time()
+asyncio.run(main())
+print(f'Время выполнения программы: {time.time() - start_time} с.')
